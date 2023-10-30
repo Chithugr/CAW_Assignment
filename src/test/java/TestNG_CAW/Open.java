809ed1520d3b02f862163ab5af067ca5bed88828
chaithragr409@gmail.com <your.email@example.com>
2023-10-30 14:34:45 +0530

    Add Package

--- src/test/java/TestNG_CAW/Open.java
+++ src/test/java/TestNG_CAW/Open.java
@@ -1,27 +1,37 @@
-package CAW;
+package TestNG_CAW;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class TC1 {
-    public static void main(String[] args) throws InterruptedException {
+public class Open {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup(){
         System.setProperty("webdriver.chrome.driver", "D:\\Automation\\Chrome\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void TC1(){
         driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        Thread.sleep(2000);
-
-
+    }
+    @Test
+    public void TC2(){
         WebElement table = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/details[1]/summary[1]"));
         table.click();
-
-        Thread.sleep(2000);
-
+    }
+    @Test
+    public void TC3(){
         WebElement input = driver.findElement(By.id("jsondata"));
         String jsondata = "[{\"name\" : \"Bob\", \"age\" : 20, \"gender\": \"male\"}, {\"name\": \"George\", \"age\" : 42, \"gender\": \"male\"}, {\"name\":\n" +
                 "\"Sara\", \"age\" : 42, \"gender\": \"female\"}, {\"name\": \"Conor\", \"age\" : 40, \"gender\": \"male\"}, {\"name\":\n" +
@@ -30,9 +40,15 @@ public class TC1 {
             input.clear();
             input.sendKeys(jsondata);
         }
-
-        driver.findElement(By.id("refreshtable")).click();
-
-        Thread.sleep(2000);
     }
+    @Test
+    public void TC4(){
+        driver.findElement(By.id("refreshtable")).click();
+    }
+
+    @AfterTest
+    public void teardown(){
+        driver.quit();
+    }
+
 }
